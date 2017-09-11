9eb19c3e6232d2d2b2a673c14779ea3fe46bab10
manjusakas <13636662054@163.com>
2017-09-11 09:17:34 +0800

    db

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,10 @@
-const login = require('./login')
+//const login = require('./login')
 const catalog = require('./catalog')
 const article = require('./article')
 
 
 module.exports = function(app){
-    app.use(login)
+    //app.use(login)
     app.use(catalog)
     app.use(article)
 
