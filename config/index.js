b22178ba4b756208f55032296b152ddd1f3c69b4
manjusakas <13636662054@163.com>
2017-09-11 12:08:14 +0800

    crypto

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 3101,
+    port: 80,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
