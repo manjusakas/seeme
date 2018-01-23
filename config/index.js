01326db21c2836fcc15445e752c91d85dfcfb46f
manjusakas <13636662054@163.com>
2018-01-23 23:11:40 +0800

    see

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 801,
+    port: 80,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
